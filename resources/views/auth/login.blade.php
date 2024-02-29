31eb2dd918cf2f797751542a9abda1e6f049eb22
Andi Gigatera Halil M <54834402+agthm3@users.noreply.github.com>
2024-02-29 14:16:44 +0800

    remove dashboard ui in login and register page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -19,24 +19,8 @@
     <div class="screen-cover d-none d-xl-none"></div>
 
     <div class="row">
-        <div class="col-12 col-lg-3 col-navbar d-none d-xl-block">
-            <aside class="sidebar">
-                <a href="#" class="sidebar-logo">
 
-                    <div class="d-flex justify-content-start align-items-center">
-                        <span>Smart Attendance</span>
-                    </div>
-
-                    <button id="toggle-navbar" onclick="toggleNavbar()">
-                        <img src="{{ asset('assets/img/global/navbar-times.svg') }}" alt="" />
-                    </button>
-                </a>
-
-
-            </aside>
-        </div>
-
-        <div class="col-12 col-xl-9">
+        <div class="col-lg-12">
             <div class="nav">
                 <div class="d-flex justify-content-between align-items-center w-100 mb-3 mb-md-0">
                     <button class="btn-notif d-block d-md-none">
