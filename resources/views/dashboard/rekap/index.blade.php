5366b44163eb9701382653411de53ed9fd4878a2
Andi Gigatera Halil M <54834402+agthm3@users.noreply.github.com>
2024-03-03 19:36:50 +0800

    add print feature

--- resources/views/dashboard/rekap/index.blade.php
+++ resources/views/dashboard/rekap/index.blade.php
@@ -1,20 +1,45 @@
 @extends('layouts.app')
 
 @section('content')
+    <style>
+        @media print {
+            body * {
+                visibility: hidden;
+            }
+
+            .document-card,
+            .document-card * {
+                visibility: visible;
+            }
+
+            .document-card {
+                position: absolute;
+                left: 0;
+                top: 0;
+            }
+        }
+    </style>
+
     <div class="content">
         <div class="row">
             <div class="col-12">
                 <a href="{{ route('home.index') }}" class="content-title">Kembali</a>
                 <!-- <h5 class="content-desc mb-4">
-                                                                                                                                                                                                                                                Your business growth
-                                                                                                                                                                                                                                            </h5> -->
+                                                                                                                                                                                                                                                                                Your business growth
+                                                                                                                                                                                                                                                                            </h5> -->
             </div>
 
 
             <div class="row mt-5">
                 <h3>Rekap Absensi</h3>
+                <div class="row">
+                    <div class="col-lg-2 mt-2 mb-3"><button onclick="printTable()" class="btn btn-primary">Cetak Rekap
+                            Absensi</button>
+                    </div>
+                </div>
                 <div class="document-card">
                     <div class="document-item">
+
                         <table class="table">
                             <thead>
                                 <tr>
@@ -43,4 +68,9 @@
                 </div>
             </div>
         </div>
+        <script>
+            function printTable() {
+                window.print();
+            }
+        </script>
     @endsection
